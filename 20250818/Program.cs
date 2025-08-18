5e2ba5c3d97eb86f7fa8942dba2cf5dfc2d5d379
seriakinori <seriakinori0108@gmail.com>
2025-08-19 00:51:20 +0900

    2025-08-18

---
+++ 20250818/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> a = ["Abc", "ABC", "abc", "aBC", "AbC"];
+        foreach (string i in a)
+        {
+            if (i.Contains('b'))
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        for (int i = 0; i < a.Count; i++)
+        {
+            if (a[i].Contains('b'))
+                Console.WriteLine(a[i]);
+        }
+
+        int j = 0;
+        while (j < a.Count)
+        {
+            if (a[j].Contains('b'))
+                Console.WriteLine(a[j]);
+            j++;
+        }
+    }
+}
