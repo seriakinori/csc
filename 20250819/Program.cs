43fb3a6a396f284d5bd5a3c48b2d5a736ab5c945
seriakinori <seriakinori0108@gmail.com>
2025-08-20 00:52:21 +0900

    2025-08-19

---
+++ 20250819/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Program20250819;
+public class Program20250819{
+    public static void Main(string[] args)
+    {
+        List<string> a = ["Abc", "ABC", "abc", "aBC", "AbC"];
+        var l = a.Where(s => s.Contains('B'));
+        foreach (string s in l) {
+            Console.WriteLine(s);
+        }
+    }
+}
