1ab77e8b4a744113c55fe5fd144484cc83009932
seriakinori <seriakinori0108@gmail.com>
2025-08-23 01:56:59 +0900

    2025-08-22

---
+++ 20250822/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void q1()
+    {
+        var val = Console.ReadLine();
+
+        if (!int.TryParse(val, out var n))
+        {
+            Console.WriteLine("cannot convert");
+            return;
+        }
+
+        int output = n switch
+        {
+            <= 100 => n * 2,
+            <= 500 => n * 3,
+            _ => n
+        };
+
+        Console.WriteLine(output);
+    }
+
+    public static void Main(string[] args){
+        q1();
+    }
+}
