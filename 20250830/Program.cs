cf8f69acefb2bb32a6be24298e91e6406c4f704c
seriakinori <seriakinori0108@gmail.com>
2025-08-30 23:24:05 +0900

    2025-08-30

---
+++ 20250830/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using System.Net.Security;
+
+public class Program{
+    public List<string> data;
+    public Program()
+    {
+        data = new List<string>
+        {
+            "AAAA","BBBB","CCCC","abcd","abCD","ABcd","aabb","ccdd"
+        };
+    }
+
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        /*
+        do
+        {
+            var name = Console.ReadLine();
+            if (string.IsNullOrEmpty(name))
+            {
+                break;
+            }
+            var index = p.data.FindIndex(s => s == name);
+            Console.WriteLine(index);
+        } while (true);
+        var count = p.data
+            .Where(s => s.Contains("ab"))
+            .ToArray();
+        foreach (var c in count) {
+            Console.WriteLine(c);
+        }
+        */
+        var selected = p.data
+            .Where(s => s.StartsWith('a'))
+            .Select(s => s.Length);
+        foreach (var length in selected)
+        {
+            Console.WriteLine(length);
+        }
+    }
+}
