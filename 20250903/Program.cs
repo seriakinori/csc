da5e8f7cfad94a23c36a2113840f62c13d76a8e6
seriakinori <seriakinori0108@gmail.com>
2025-09-04 00:59:25 +0900

    2025-09-03

---
+++ 20250903/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+using System.Text;
+
+class Program6
+{
+    static void q2()
+    {
+        string s = Console.ReadLine();
+        if (int.TryParse(s, out int var))
+        {
+            Console.WriteLine(var);
+        }
+        else
+        {
+            Console.WriteLine("Convert failed");
+        }
+    }
+
+    static void q3()
+    {
+        string s = "Jackdaws love my big sphinx of quartz";
+        //Console.WriteLine(s.Count(si => si == ' '));
+        //Console.WriteLine(s.Replace("big", "small"));
+        /*
+        var array = s.Split(" ").ToArray();
+        StringBuilder sb = new StringBuilder(array[0]);
+        foreach (string s_i in array.Skip(1))
+        {
+            sb.Append(' ');
+            sb.Append(s_i);
+        }
+        Console.WriteLine(sb);
+        */
+        //Console.WriteLine(s.Split(" ").ToArray().Length);
+        var sp = s.Split(" ");
+        var less4 = sp.Where(w => w.Length < 5);
+        foreach(string s_i in less4)
+            Console.WriteLine(s_i);
+    }
+
+    static void Main(string[] args)
+    {
+        q3();
+    }
+    
+}
