4f539b01f2dfaec5299dd6602545459350332007
seriakinori <seriakinori0108@gmail.com>
2025-09-18 23:56:44 +0900

    2025-09-18

---
+++ 20250918/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+
+class Practice
+{
+    static void q10_1_1(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            return;
+        }
+
+        using var reader = new StreamReader(filePath);
+        int cnt = 0;
+        string line = "";
+        while (!reader.EndOfStream)
+        {
+            line = reader.ReadLine();
+            if (line.Contains("class"))
+            {
+                Console.WriteLine(line);
+                cnt++;
+            }
+        }
+
+        Console.WriteLine(cnt);
+    }
+
+    static void q10_1_2(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            return;
+        }
+
+        var lines = File.ReadAllLines(filePath);
+        int cnt = 0;
+        foreach (var line in lines)
+        {
+            if (line.Contains("class"))
+            {
+                Console.WriteLine(line);
+                cnt++;
+            }
+        }
+        Console.WriteLine(cnt);
+    }
+
+    static void q10_1_3(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            return;
+        }
+
+        var cnt = File.ReadLines(filePath).Count(s => s.Contains("class"));
+        Console.WriteLine(cnt);
+    }
+
+    static void q10_2(string inputPath, string outputPath)
+    {
+        using var reader = new StreamReader(inputPath);
+        using var writer = new StreamWriter(outputPath);
+        int cnt = 1;
+        string outputContent = "";
+        while (!reader.EndOfStream)
+        {
+            outputContent = reader.ReadLine();
+            writer.WriteLine("{0}:{1}",cnt.ToString("00"),outputContent);
+            cnt++;
+        }
+    }
+
+    static void q10_3(string filePath1, string filePath2)
+    {
+        using var reader = new StreamReader(filePath1);
+        using var writer = new StreamWriter(filePath2, append:true);
+        string outputContent = "";
+        while (!reader.EndOfStream)
+        {
+            outputContent = reader.ReadLine();
+            writer.WriteLine(outputContent);
+        }
+    }
+
+    static void q10_4()
+    {
+        var d1 = new DirectoryInfo("./d1");
+        var files = d1.EnumerateFiles();
+        foreach (var file in files)
+        {
+            if (file.Length > 1048576)
+                Console.WriteLine(file.Name);
+        }
+        var subdirs = d1.EnumerateDirectories();
+        foreach (var d2 in subdirs)
+        {
+            var files2 = d2.EnumerateFiles();
+            foreach(var file2 in files2)
+                if (file2.Length > 1048576)
+                    Console.WriteLine(file2.Name);
+        }
+    }
+
+    static void q10_5(string filePath)
+    {
+        var d1 = new DirectoryInfo(filePath);
+        var d1_files = new List<string>();
+        foreach (var f1 in d1.EnumerateFiles())
+        {
+            d1_files.Add(f1.Name);
+        }
+        foreach (var f2 in d1_files)
+        {
+            File.Copy("d1/" + f2,"d2/" + Path.GetFileNameWithoutExtension(f2) + "_bak" + Path.GetExtension(f2));
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        //q10_1_1("target.cs");
+        //q10_1_2("target.cs");
+        //q10_1_3("target.cs");
+        //q10_2("target.cs", "indexed_output.txt");
+        //q10_3("aaaa.txt", "abab.txt");
+        //q10_4();
+        q10_5("d1");
+    }
+}
---
+++ 20250918/target.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+
+
+public class Book
+{
+    public string Title { get; init; } = string.Empty;
+    public int Price { get; init; }
+    public int Pages { get; init; }
+}
+
+class Program
+{
+    public static void q7_1()
+    {
+        int[] n = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+        // 1
+        //Console.WriteLine(n.Max(s => s));
+        // 2 
+        //n.TakeLast(2).ToList().ForEach(Console.WriteLine);
+        // 3 
+        //n.Select(i => i.ToString("000")).ToList().ForEach(Console.WriteLine);
+        // 4
+        //n.Order().Take(3).ToList().ForEach(Console.WriteLine);
+        // 5
+        //Console.WriteLine(n.Distinct().Count(i => i > 10));
+    }
+
+    public static void q7_2()
+    {
+        var books = new List<Book> {
+            new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
+            new Book { Title = "ラムダ式とLINQの極意", Price = 2500, Pages = 312 },
+            new Book { Title = "ワンダフル・C#ライフ", Price = 2900, Pages = 385 },
+            new Book { Title = "一人で学ぶ並列処理プログラミング", Price = 4800, Pages = 464 },
+            new Book { Title = "フレーズで覚えるC#入門", Price = 5300, Pages = 604 },
+            new Book { Title = "私でも分かったASP.NET Core", Price = 3200, Pages = 453 },
+            new Book { Title = "楽しいC#プログラミング教室", Price = 2540, Pages = 348 },
+        };
+        // 1
+        /*
+        var a1 = books.Where(b => b.Title == "ワンダフル・C#ライフ");
+        foreach (Book b in a1)
+            Console.WriteLine("{0},{1}", b.Price, b.Pages);
+        */
+        // 2
+        //Console.WriteLine(books.Count(b => b.Title.Contains("C#")));
+        // 3
+        //books.Where(b => b.Title.Contains("C#")).Select(b => b.Title).ToList().ForEach(Console.WriteLine);
+        //Console.WriteLine(books.Where(b => b.Title.Contains("C#")).Average(b => b.Price));
+        // 4
+        //Console.WriteLine(books.Where(b => b.Price >= 4000).First().Title);
+        // 5
+        //Console.WriteLine(books.Where(b => b.Price < 4000).Max(b => b.Pages));
+        // 6
+        /*
+        foreach (Book bk in books.Where(b => b.Pages >= 400).OrderByDescending(b => b.Price))
+        {
+            Console.WriteLine("Title:{0}, Price:{1}, Pages:{2}", bk.Title, bk.Price, bk.Pages);
+        }
+        */
+        // 7
+        books.Where(b => b.Title.Contains("C#") && b.Pages <= 500).Select(b => b.Title).ToList().ForEach(Console.WriteLine);
+
+    }
+
+    static void main()
+    {
+        //q7_1();
+        q7_2();
+        return;
+    }
+}
