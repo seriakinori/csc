43d55359c26ef68f4b274e5ad49efa639b3c612f
akinoriseri <seria8614292437@gmail.com>
2025-10-07 00:15:12 +0900

    2025-10-06

---
+++ 20251006/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+class Program
+{
+    static bool code11_6(string line)
+    {
+
+        var pattern = @"([0-9A-Za-z\p{IsHiragana}\p{IsKatakana}])([0-9A-Za-z\p{IsHiragana}\p{IsKatakana}])([0-9A-Za-z\p{IsHiragana}\p{IsKatakana}])\2\1";
+        return Regex.IsMatch(line, pattern);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(code11_6("madam"));
+        Console.WriteLine(code11_6("12321"));
+        Console.WriteLine(code11_6("トマト"));
+        Console.WriteLine(code11_6("しんぶんし"));
+        Console.WriteLine(code11_6("シンブンシ"));
+    }
+}
