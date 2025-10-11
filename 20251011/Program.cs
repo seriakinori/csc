432fc1975e9a57bec11729dc1613d9be197a0a04
akinoriseri <seria8614292437@gmail.com>
2025-10-11 23:17:59 +0900

    2025-10-11

---
+++ 20251011/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Xml.Schema;
+
+public record Novel
+{
+    //[XmlElement(ElementName = "title")]
+    [XmlArray("titles")]
+    [XmlArrayItem("title", typeof(string))]
+    public string[] Titles { get; set; } 
+
+    [XmlElement(ElementName = "author")]
+    public string Author { get; set; } = string.Empty;
+
+    /*
+    [XmlElement(ElementName = "published")]
+    public int PublishedYear { get; set; } 
+    */
+}
+
+public class Program
+{
+    public static  void Main(string[] args)
+    {
+        var novel = new Novel
+        {
+            Author = "aaaa",
+            Titles = new string[]
+            {
+                "AAAA","AaAa","aAaA"
+            },
+            //PublishedYear = 9999,
+        };
+        using (var writer = XmlWriter.Create("novel.xml")) 
+        {
+            var serializer = new XmlSerializer(novel.GetType());
+            serializer.Serialize(writer, novel);
+        }
+    }
+
+}
