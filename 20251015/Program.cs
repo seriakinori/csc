a6e24280c5631597117af6044c1ead5956987108
akinoriseri <seria8614292437@gmail.com>
2025-10-16 01:57:51 +0900

    2025-10-15

---
+++ 20251015/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;// See https://aka.ms/new-console-template for more information
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+class Program{
+    public static void Main(string[] args){
+        return;
+    }
+}
