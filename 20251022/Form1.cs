5799654610be69c6e65c94ca22c43fe5e024f35b
akinoriseri <seria8614292437@gmail.com>
2025-10-22 23:59:12 +0900

    2025-10-22

---
+++ 20251022/Form1.cs
@@ -0,0 +1,43 @@
+namespace _20251022;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+    private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        label1.Text = "";
+        var th = new Thread(DoSomething);
+        th.Start();
+    }
+
+    private void DoSomething()
+    {
+        DoLongTimeWork();
+        CancellationToken token = _cancellationTokenSource.Token;
+
+        if (token.IsCancellationRequested)
+        {
+            return;
+        }
+
+        label1.Invoke((Action)delegate (){
+            label1.Text = "completed";
+        });
+    }
+
+    private void DoLongTimeWork()
+    {
+        System.Threading.Thread.Sleep(5000);
+    }
+}
